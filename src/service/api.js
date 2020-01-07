a457b72f59b5d2a62399fb49ef8214dd8336a8ba
zhangyanlong <yanlongzhang1990@gmail.com>
2020-01-07 17:43:34 +0800

    公网ip

--- src/service/api.js
+++ src/service/api.js
@@ -1,4 +1,4 @@
-const HOST = process.env.NODE_ENV === 'development' ? '/api' : 'http://172.17.0.14/api'
+const HOST = process.env.NODE_ENV === 'development' ? '/api' : 'http://49.235.174.250/api'
 export default {
   home: {
     getHomeInfo: `${HOST}/getHomeInfo`
