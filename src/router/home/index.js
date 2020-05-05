d7fad1fa62603c2f5539ea62b90f1a422f52ae7e
zhangyanlong <zhangyanlong@basestonedata.com>
2020-05-05 10:28:24 +0800

    测试服分支测试

--- src/router/home/index.js
+++ src/router/home/index.js
@@ -3,6 +3,6 @@ export default [
     path: '/',
     name: 'home',
     component: resolve => require(['@/views/home/index'], resolve),
-    meta: { index: 0 }
+    meta: { index: 0, title: '百里小站测试服' }
   }
 ]
